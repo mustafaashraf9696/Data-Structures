7248dd276400cc855c7dacc704fe83864031b08b
Mustafa Ashraf <45900814+mustafaashraf9696@users.noreply.github.com>
2020-03-11 19:24:36 +0300

    Update Stack.h

--- Stacks-Array-based-implementation/Stack.h
+++ Stacks-Array-based-implementation/Stack.h
@@ -2,26 +2,26 @@
 #define STACK_H_INCLUDED
 
 #define  MAXSTACK 100
-typedef int StackEntery   ;
+typedef int StackEntry   ;
 
 
 typedef struct stack {
 int top ;
-StackEntery entery [MAXSTACK];
+StackEntry entry [MAXSTACK];
 }Stack;
 
 //Stack Implementation
 
 
 void CreateStack (Stack *ps);
-int Push (StackEntery e , Stack *Ps);
+int Push (StackEntry e , Stack *Ps);
 int StackFull (Stack *ps);
-void Pop (StackEntery *pe , Stack *ps);
+void Pop (StackEntry *pe , Stack *ps);
 int StackEmpety (Stack *ps);
-void StackTop (StackEntery *pe ,Stack *ps);
+void StackTop (StackEntry *pe ,Stack *ps);
 int StackSize (Stack *ps);
 void ClearStack (Stack *ps);
-void TraverseStack (Stack *ps , void(*pf)(StackEnery));
+void TraverseStack (Stack *ps , void(*pf)(StackEnry));
 
 
 #endif // STACK_H_INCLUDED
